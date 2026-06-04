93780e27b7c5bc98a37e175cba8f35be95c38ad8
gslsethu <gslsethu@gmail.com>
2026-06-04 09:27:33 +0530

    Time: 3205 ms (5.38%), Space: 13.1 MB (80.94%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -4,19 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        if len(nums)==0:
-            return None
-        if len(nums)<2:
-            return nums
-        if len(nums)==2:
-            if nums[0]!=nums[1]:
-                return nums
-            else:
-                return None
+        
         ans=[]
-        if len(nums)>2:
-            for i in nums:
-                if nums.count(i)==1:
-                    ans.append(i)
-            return ans
+        
+        for i in nums:
+            if nums.count(i)==1:
+                 ans.append(i)
+        return ans
 
