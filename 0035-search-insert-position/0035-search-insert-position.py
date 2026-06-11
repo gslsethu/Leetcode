af322b406a3bc87d8ae809100f749ea9549ae6e2
gslsethu <gslsethu@gmail.com>
2026-06-11 12:40:10 +0530

    Time: 3 ms (4.83%), Space: 13 MB (30.45%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -2,10 +2,9 @@ class Solution(object):
     def searchInsert(self, nums, target):
         length=len(nums)
         for i in range(length):
-            
             if nums[i]>=target:
                 return i
             
-        return len(nums)
+        return length
 
         
