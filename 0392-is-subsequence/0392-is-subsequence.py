bcd2b3e962e7f095ef89a87ce46f3f5c0ee2e05f
gslsethu <gslsethu@gmail.com>
2026-07-14 11:33:38 +0530

    Time: 0 ms (100%), Space: 12.4 MB (46.65%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        left=0
+        right=0
+        while left<len(s) and right<len(t):
+            if s[left]==t[right]:
+                left+=1
+                right+=1
+            else:
+                right+=1
+        if left==len(s):
+            return True
+        else: 
+            return False
+        
+       
+                
+
+        
+        
