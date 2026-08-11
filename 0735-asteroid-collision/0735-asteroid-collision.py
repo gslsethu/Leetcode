a089f9b4e6605282481e7ecd7bc6a841c56178b1
gslsethu <gslsethu@gmail.com>
2026-08-11 12:10:16 +0530

    Time: 12 ms (22.67%), Space: 13.2 MB (77.28%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        stack=[]
+        for i in range(len(asteroids)):
+            alive=True
+            while stack and stack[-1]>0 and asteroids[i]<0:
+                if stack[-1]<abs(asteroids[i]):
+                    stack.pop()
+                elif stack[-1]==abs(asteroids[i]):
+                    stack.pop()
+                    alive=False
+                    break
+                   
+                else:
+                    alive=False
+                    break
+                   
+            if alive:
+                stack.append(asteroids[i])
+            
+        return stack
+        
