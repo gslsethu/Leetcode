f56c15ba4d23fc667b897b3b82da00fbeba7c7d9
gslsethu <gslsethu@gmail.com>
2026-09-07 13:30:51 +0530

    Time: 7 ms (26.25%), Space: 23.5 MB (16.21%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -12,11 +12,11 @@ class Solution:
                 five -= 1
                 ten += 1
 
-            else:  # $20
+            else:  
                 if ten > 0 and five > 0:
                     ten -= 1
                     five -= 1
-                elif five >= 3:
+                elif five >2:
                     five -= 3
                 else:
                     return False
